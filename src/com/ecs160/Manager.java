ae0e1c5d97cf9cbeebb893d0dc47810720a2e516
Daniel Woodall <dtwoodall@gmail.com>
2011-03-29 16:19:27 -0700

    committed android project files

---
+++ src/com/ecs160/Manager.java
@@ -0,0 +1,13 @@
+package com.ecs160;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Manager extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
